c5f1dfd73153a9ae29c3841f37dd3047c59a21e1
madjax <madjax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 20:40:26 +0000

    1.2.6
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-rejoiner/trunk@1194423 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-rejoiner.php
+++ woocommerce-rejoiner.php
@@ -5,7 +5,7 @@ Plugin URI: http://jacksonwhelan.com/woocommerce-rejoiner/
 Description: Integrates Rejoiner with WooCommerce.
 Author: Jackson Whelan
 Author URI: http://www.jacksonwhelan.com
-Version: 1.2.5
+Version: 1.2.6
 */
 
 // Add the integration to WooCommerce
